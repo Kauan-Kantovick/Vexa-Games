1d23212a936cc5647c5aac583ebc534a754be822
Kauan K. D. da Silva <k.kantovick2008@gmail.com>
2026-09-16 10:28:55 -0300

    feat: Navigating between pages

--- src/pages/Statics/Cart.jsx
+++ src/pages/Statics/Cart.jsx
@@ -1,7 +1,12 @@
+import { Link } from "react-router"
+
 function Cart() {
   return (
     <>
       <h1>Cart</h1>
+
+      <Link to="/">View Catalog</Link>
+
     </>
   )
 }
--- src/pages/Statics/Catalog.jsx
+++ src/pages/Statics/Catalog.jsx
@@ -1,7 +1,11 @@
+import { Link } from "react-router"
+
 function Catalog() {
   return (
     <>
       <h1>Catalog</h1>
+
+      <Link to="/cart">View Cart</Link>
     </>
   )
 }
